70ba66d515b9f18541ea06a6b565471fa34c7c80
Jhonata Nogueira <jhonata@flashnetbrasil.com.br>
2022-08-25 20:23:21 -0300

    Modal & Form
    Modal utilizando react-modal, formulário, estado dos botões e input, listagem de transações e formatando valores com Intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import styled from 'styled-components'
+import Modal from 'react-modal'
+import { useState } from 'react'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header/Header';
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-`
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}      
+      />
+
       <GlobalStyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -50,4 +50,54 @@ import { createGlobalStyle } from 'styled-components'
         opacity: 0.6;
         cursor: not-allowed;
     }
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+        transition: filter 0.3s;
+
+        &:hover {
+            filter: brightness(0.8);
+        }
+    }
+
+    .ReactModal__Overlay {
+    opacity: 0;
+    transform: translateY(-20px);
+    transition: all 0.3s ease-in-out;
+    }
+
+    .ReactModal__Overlay--after-open{
+        opacity: 1;
+        transform: translateY(0px);
+    }
+
+    .ReactModal__Overlay--before-close{
+        opacity: 0;
+        transform: translateY(-20px);
+    }
 `
