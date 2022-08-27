10fe157fb263f5d622858d763345ca6a0a026e76
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-08-27 09:36:49 -0300

    Atualização Rota MirageJs
    Netlify

--- src/index.tsx
+++ src/index.tsx
@@ -34,11 +34,11 @@ createServer({
   routes() {
     this.namespace = 'api';
 
-    this.get('/transactions', () => {
+    this.get('https://dapper-bublanina-217977.netlify.app/api/transactions', () => {
       return this.schema.all('transaction')
     })
 
-    this.post('/transactions', (schema, request) => {
+    this.post('https://dapper-bublanina-217977.netlify.app/api/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
       return schema.create('transaction', data);
